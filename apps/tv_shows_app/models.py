c0d64aa5ee2678c609a07f27b35011396ced0968
Isabel Galdamez <isabelgaldamez@sbcglobal.net>
2019-09-18 12:10:35 -0700

    added validations

--- apps/tv_shows_app/models.py
+++ apps/tv_shows_app/models.py
@@ -1,4 +1,17 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import date
+
+class BlogManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = 'Show title must have more than 2 chars'
+        elif len(postData['network_name']) < 3:
+            errors['network_name'] = 'Network name must be longer than 3 characters'
+        elif len(postData['show_desc']) < 10:
+            errors['description'] = 'Show description should be longer than 10 chars'
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
@@ -7,4 +20,5 @@ class Show(models.Model):
     release_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-# Create your models here.
+    objects = BlogManager()
+
--- apps/tv_shows_app/views.py
+++ apps/tv_shows_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
 from .models import Show
 
 def index(request):
@@ -14,7 +15,13 @@ def show_new(request):
     return render(request, 'tv_shows_app/add_shows.html')
 
 def create(request):
-    if request.method == "POST":
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect ('shows/new')
+
+    else: 
         show_title = request.POST['title']
         show_network = request.POST['network_name']
         show_date = request.POST['release_date']
@@ -22,7 +29,7 @@ def create(request):
         Show.objects.create(title=show_title, network=show_network, release_date= show_date, desc=show_description)
         show= Show.objects.get(title = show_title)
         show_id = show.id
-    return redirect(f'shows/{show_id}')
+        return redirect(f'shows/{show_id}')
 
 def display(request, show_id):
     context = {
@@ -31,29 +38,35 @@ def display(request, show_id):
     return render(request, 'tv_shows_app/display_shows.html', context)
 
 def edit(request, show_id):
+    show = Show.objects.get(id = show_id)
     context = {
+        "show_info" : Show.objects.get(id = show_id),
+        "release_date" : show.release_date.strftime("%Y-%m-%d"),
         "show_id" : show_id
     }
     return render(request, 'tv_shows_app/edit.html', context)
 
 def update(request, show_id):
     update_show = Show.objects.get(id = show_id)
-    if request.method == 'POST':
-        if len(request.POST['title']) != 0 and update_show.title !=  request.POST['title']:
-            update_show.title = request.POST['title']
-            print(update_show.title)
-            update_show.save()
-        elif len(request.POST['network_name']) != 0 and update_show.network != request.POST['network_name']:
-            update_show.network = request.POST['network_name']
-            update_show.save()
-        elif len(request.POST['release_date']) != 0 and update_show.release_date != request.POST['release_date']:
-            update_show.release_date = request.POST['release_date']
-            update_show.save()
-        elif len(request.POST['show_desc']) != 0 and update_show.desc != request.POST['show_desc']:
-            update_show.desc = request.POST['show_desc']
-            update_show.save()
+    errors = Show.objects.basic_validator(request.POST)
 
-
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect (f'/shows/{show_id}/edit')
+    
+    if len(request.POST['title']) != 0 and update_show.title !=  request.POST['title']:
+        update_show.title = request.POST['title']
+        update_show.save()
+    if len(request.POST['network_name']) != 0 and update_show.network != request.POST['network_name']:
+        update_show.network = request.POST['network_name']
+        update_show.save()
+    if len(request.POST['release_date']) != 0 and update_show.release_date != request.POST['release_date']:
+        update_show.release_date = request.POST['release_date']
+        update_show.save()
+    if len(request.POST['show_desc']) != 0 and update_show.desc != request.POST['show_desc']:
+        update_show.desc = request.POST['show_desc']
+        update_show.save()
     return redirect(f'/shows/{show_id}')
 
 def destroy(request, show_id):
